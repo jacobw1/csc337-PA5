f13bf4cd9bf64531b02740a0d5cd9b6997bfcb28
Jacob Williams <jacobw1@email.arizona.edu>
2021-09-16 13:27:55 -0700

    fixed oninput js

--- js/pa5.js
+++ js/pa5.js
@@ -2,8 +2,8 @@ var input_text = document.getElementById("text_field");
 var ceasar_out = document.getElementById("caesar_cipher");
 var square_out = document.getElementById("square_cipher");
 
-input_text.onInput = function() {
-  caesar_out.innerText = input_text.value;
+input_text.oninput = function() {
+  ceasar_out.innerText = input_text.value;
 }
 
 console.log("finished running js");
